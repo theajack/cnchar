078676b5304ef594822489737eaee4d05b2cd147
tackchen <tackchen@tencent.com>
2020-04-14 19:37:48 +0800

    docs: 增加mta数据统计

--- docs/.vuepress/components/home.vue
+++ docs/.vuepress/components/home.vue
@@ -52,6 +52,7 @@
 </template>
 
 <script>
+
     export default {
         data () {
             return {
--- helper/build-config.js
+++ helper/build-config.js
@@ -1,16 +1,28 @@
 const gulp = require('gulp');
-// const fs = require('fs');
+const fs = require('fs');
 const babel = require('gulp-babel');
 
 function main () {
     gulp.src('public/config.js')
         .pipe(babel({presets: ['@babel/env']}))
         .pipe(gulp.dest('docs'));
+    addMta();
 }
 
-// function modPath () {
-//     var html = fs.readFileSync('./docs/assets/v2/index.html', 'utf8');
-//     html = html.replace(/\/cnchar\/assets\//g, '/cnchar/assets/v2/assets/');
-//     fs.writeFileSync('./docs/index.html', html, 'utf8');
-// }
+function addMta () {
+    let mta = /* html*/`<script>
+var _mtac = {};
+(function () {
+    var mta = document.createElement("script");
+    mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+    mta.setAttribute("name", "MTAH5");
+    mta.setAttribute("sid", "500700068");
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(mta, s);
+})();
+</script>`;
+    var html = fs.readFileSync('./docs/v2/index.html', 'utf8');
+    html = html.replace('</body>', mta + '</body>');
+    fs.writeFileSync('./docs/v2/index.html', html, 'utf8');
+}
 main();
