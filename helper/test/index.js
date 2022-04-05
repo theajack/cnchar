e901e6a62eeb8120a6acdd206696a19316c35bd2
tackchen <tackchen@tencent.com>
2022-04-05 22:52:25 +0800

    feat: 1

--- helper/test/index.js
+++ helper/test/index.js
@@ -1,10 +1,10 @@
-const cnchar = require('../../npm/packages/cnchar');
-const order = require('../../npm/packages/order');
-const trad = require('../../npm/packages/trad');
-const poly = require('../../npm/packages/poly');
-const idiom = require('../../npm/packages/idiom');
-const xhy = require('../../npm/packages/xhy');
-const radical = require('../../npm/packages/radical');
+const cnchar = require('../../npm/packages/cnchar/cnchar.min');
+const order = require('../../npm/packages/order/cnchar.order.min');
+const trad = require('../../npm/packages/trad/cnchar.trad.min');
+const poly = require('../../npm/packages/poly/cnchar.poly.min');
+const idiom = require('../../npm/packages/idiom/cnchar.idiom.min');
+const xhy = require('../../npm/packages/xhy/cnchar.xhy.min');
+const radical = require('../../npm/packages/radical/cnchar.radical.min');
 
 console.log('****TEST NPM****');
 console.log();
