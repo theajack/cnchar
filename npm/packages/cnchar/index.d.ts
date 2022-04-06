0628de2c073e043215e5142dd741d2de8b033e35
tackchen <tackchen@tencent.com>
2022-04-06 09:21:17 +0800

    feat: push del tag

---
+++ npm/packages/cnchar/index.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: tackchen
+ * @Date: 2022-04-06 09:20:49
+ * @LastEditors: tackchen
+ * @LastEditTime: 2022-04-06 09:20:50
+ * @FilePath: /cnchar/npm/packages/cnchar/index.d.ts
+ * @Description: Coding something
+ */
